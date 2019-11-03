18f1b22e4c061f5665e35d1c1629b02f4673b0e7
Duval <ngounoukenhem@gmail.com>
2019-11-03 21:08:01 +0100

    header on page

---
+++ header.php
@@ -0,0 +1,16 @@
+<header class=" col-12  ">
+                <div class="logo"><a href="index.php">Exodus</a></div>
+                <ul>
+                    <li class="button  show_menu "> + </li>
+                </ul>
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="../controller/router.php">Accueil</a></li>
+                        <li><a href="#Actu">Actu</a></li>
+                        <li><a href="../public/navigation/aPropos/about.php">A Propos</a></li>
+                        <li><a href="../public/navigation/contact/contact.php">Contact</a></li>
+                    </ul>
+
+                </div>
+
+</header>
