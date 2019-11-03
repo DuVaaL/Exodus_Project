18dde23a3f7afe73fcd894675a54d176e687b2c7
Duval <ngounoukenhem@gmail.com>
2019-11-03 21:05:38 +0100

    folder App

---
+++ App/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+	<header>
+		<nav>
+			<ul class="item-nav">
+				<li style="list-style: none"> <a href="home.php">Home </a></li>
+				<li style="list-style: none"> <a href="#about">About </a></li>
+				<li style="list-style: none"> <a href="#footer">Contact </a></li>
+			</ul>
+		</nav>
+	</header>
+</div>
