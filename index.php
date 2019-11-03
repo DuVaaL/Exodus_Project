dbda6904c9f73d093d76ca0a954e087cfa41394c
Duval <ngounoukenhem@gmail.com>
2019-11-03 21:10:58 +0100

    This files is router on website

---
+++ index.php
@@ -0,0 +1,231 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> Exodus · Site d'information hightech </title>
+    <link rel="stylesheet" href="public/style.css">
+    <link rel="icon" href="public/img/logomoac.png">
+    <link rel="stylesheet" href="public/bootstrap-grid.css">
+    <link rel="stylesheet" href="public/bootstrap.css">
+    <link rel="stylesheet" href="public/fontawesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="">
+    <meta name="description" content="hightech, android, smartphone">
+</head>
+
+<body>
+    <div id="main-bloc" class="container-fluid ">
+        <div class="row">
+            <?php include "header.php"; ?>
+        </div>
+    </div>
+
+    <div id="main_page">
+        <div class="container" >
+          <div class="row">
+              <ul class="all_items" >
+                  <div class="pic_1" >
+                      <li >
+                     <div class="large-item " >
+                            <a href="#">
+                             <div class="box_img_1" >
+                                 
+                                  <h3 class="title_item"> Migrer chez Apple ? </h3>
+                             </div>
+                            </a>
+                     </div>
+                      
+                      </li>
+                      <li >
+                     <div class="large-item " >
+                            <a href="#">
+                             <div class="box_img_2" >
+                                 
+                                  <h3 class="title_item"> Les Airpods valent'ils vraiment la peine ? </h3>
+                             </div>
+                            </a>
+                     </div>
+                      
+                      </li>
+                  </div>
+                  <div class="pic_2" >
+                      <li >
+                     <div class="large-item " >
+                            <a href="#">
+                             <div class="box_img_3" >
+                                 
+                                  <h3 class="title_item"> setups : Mes coups de coeur de la semaine </h3>
+                             </div>
+                            </a>
+                     </div>
+                      
+                      </li>
+                      <li >
+                     <div class="large-item " >
+                            <a href="#">
+                             <div class="box_img_4" >
+                                 
+                                  <h3 class="title_item"> Gadgets Hightech </h3>
+                             </div>
+                            </a>
+                     </div>
+                      
+                      </li>
+                  </div>
+                  
+              </ul>
+          </div>
+      </div>
+       
+        <div class="container" style="margin-top : 20px;">
+            <div class="row">
+                <div class="sections" >
+                    <section id="contents">
+                        <div class="head">
+                            <h2 class="title-content" id="Actu" >Dernières Actus</h2>
+                        </div>
+                        <div class="line-up"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/huawei_240x170.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Huawei a deja vendu plus de 200 millions de smartphones en 2019.</a>
+                                </h3>
+                                <p>
+                                    Huawei a deja vendu plus de 200 millions de smartphones en 2019 malgrés les mesures prises par l'embargo américain, le géant chinois a réussi à battre le record qu'il s'est fixé en 2018...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/phillips_240x170.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Phillips : la nouvelle sortie prévue pour le 1er Novembre.</a>
+                                </h3>
+                                <p>
+                                    C'est officiel Phillips nous enverra du lourd dès novembre avec la nouvelle sortie de son téléviseur phare...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/facebook-amende-reconnaissance-faciale.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Facebook risque une amende de 35 milliards de dollars.</a>
+                                </h3>
+                                <p>
+                                    Facebook risque une amende record de 35 milliards de dollars pour avoir fait un usage abusif de la reconnaissance faciale. Un recours lancé aux Etats-Unis accuse en effet le réseau social d’avoir utilisé les photos de 7 millions d’internautes pour analyser leurs visages sans leur consentement...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/huawei-android.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Huawei ne cherche plus à remplacer Android : trop risqué et lointain.</a>
+                                </h3>
+                                <p>
+                                    Huawei ne cherche plus à se passer d’Android : l’un des dirigeants de la firme, Joy Tan, confirme que les prochains smartphones de la firme tourneront tous sur le système d’exploitation maintenu par Google. Néanmoins le constructeur poursuit ses efforts...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/huawei-mate-xs.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Huawei annonce déjà le Mate Xs, son 2ème smartphone pliable.</a>
+                                </h3>
+                                <p>
+                                    Huawei vient déjà de livrer un premier aperçu de son Mate Xs, son deuxième smartphone pliable. Contrairement au Mate X, le futur flagship est doté du SoC Kirin 990 des Mate 30. Le groupe chinois lancera cette seconde version, très similaire à la première, dans le courant du mois de mars 2020...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/huawei-mate-x.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Huawei Mate X : lancement le 15 novembre à 2400 $, c’est officiel.</a>
+                                </h3>
+                                <p>
+                                    Après plusieurs reports, le Huawei Mate X sera finalement disponible le 15 novembre en Chine. Le constructeur vient de l’annoncer au cours d’une conférence. Le smartphone est déjà disponible en pré-commande au prix de 16,999 yuan, soit environ 2400 $ pour 8 Go de RAM et 512 Go de stockage...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/iphone-11-11-pro-resistance-eau.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">iPhone 11 et 11 Pro : ce test de résistance à l’eau montre qu’ils sont insubmersibles.</a>
+                                </h3>
+                                <p>
+                                    Les iPhone 11 et 11 Pro ont subi un test de l’extrême visant à déterminer leur degré de résistance à l’eau. Ils ont été plongés jusqu’à 12 mètres de profondeur pendant 30 minutes dans une baie, soit 3 fois la profondeur maximale recommandée par Apple...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/samsung-exynos.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">Samsung spoile un nouveau SoC Exynos et promet un bond en avant.</a>
+                                </h3>
+                                <p>
+                                    Samsung va officialiser un nouveau SoC Exynos lors de son événement Tech Day 2019 qui se tiendra à San Francisco. La puce est teasée sur Twitter dans une courte vidéo qui ne révèle pas beaucoup d’indices sur le SoC si ce n’est qu’il fera la part belle à l’intelligence artificielle...
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <article>
+                            <div class="img-content">
+                                <a href=""> <img src="public/img/samsung-exynos-990-galaxy-s11.jpg" alt=""> </a>
+                            </div>
+                            <div class="text-box">
+                                <h3 class="post-title" >
+                                    <a href="#">
+                                    Samsung présente l’Exynos 990, le SoC 5G du futur Galaxy S11.</a>
+                                </h3>
+                                <p>
+                                    Samsung vient de lever le voile sur l’Exynos 990, une nouvelle génération de SoC gravés en 7nm. Comme le Kirin 990 de Huawei, la puce se distingue par l’intégration d’un modem 5G sur le même bout de silicium. Ce système est optimisé pour les capteurs photo 108 Mp.
+                                </p>
+                            </div>
+                        </article>
+                        <div class="line-down"></div>
+                        <div class="more-article" >
+                            <a href=""><h4>Plus d'articles</h4></a>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div> 
+        
+    </div>
+    
+        <?php include "footer.php" ; ?>
+        
+    <script type="text/javascript" src="public/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="public/js/main.js"></script>
+</body>
+
+</html>
