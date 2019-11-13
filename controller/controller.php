abde15110d1a862f6709c6025180afe27e2ae091
Duval <ngounoukenhem@gmail.com>
2019-11-13 03:02:41 +0100

    update controller

--- controller/controller.php
+++ controller/controller.php
@@ -1,11 +1,16 @@
 <?php
-require('../model/model.php');
+require('model/model.php');
 
 function listArticles()
 {
 	$posts = getArticles();
+	require('view/listArticlesView.php');
+}
 
-	require('../view/listArticlesView.php');
+function other_page()
+{
+	$page = getOther_page();
+	require('view/allArticlesView.php');
 }
 
 function article()
@@ -13,13 +18,18 @@ function article()
 	$post_article = getArticle($_GET['id']);
 	$posts_comments = getComments($_GET['id']);
 
-	require('../view/listPostComments.php');
+	require('view/listPostComments.php');
 }
 
-// function contentArticle()
-// {
-// 	$postContent = getContentArticle($_GET['id']);
-// 	$posts_comments = getComments($_GET['id']);
+function add_comment($id_article, $author, $comment, $email)
+{
+    $result = post_comment($id_article, $author, $comment, $email);
 
-// 	require('../view/')
-// }
+    if ($result === true) {
+        
+        header('Location: index.php?action=article&id=' . $id_article);
+    }
+    else {
+       die('Impossible d\'ajouter le commentaire !');
+    }
+}
